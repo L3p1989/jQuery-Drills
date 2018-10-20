4ee853a5d030660591a92f8a37e1616758ebf4ab
Patrick Hogan <phogan110@live.com>
2018-10-20 15:51:21 -0600

    added spacing

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 $('#btnSubmit').on('click', function() {
-    alert('You got me!');
+    var value = $(this).val();
+    $('input'['type = text']).alert(value);
 });//this sends out an alert when #btnSubmit is clicked
+
