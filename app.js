8b30dd76c9607bdf32ef9ccba5bf131e449ac0b6
Patrick Hogan <phogan110@live.com>
2018-10-20 17:52:22 -0600

    added 'disabled' `attr` to `#btnSubmit`; added `keyup` function on 'input[type=text]' to ammend 'disabled' `attr` to false

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+$('#btnSubmit').attr('disabled', true);
+
+$('input[type="text"]').on('keyup', function() {
+    $('#btnSubmit').attr('disabled', false);
+});
+
 $('#btnSubmit').on('click', function() {
-    $('input[type="text"]').val(function( i, val ) {
+    $('input[type="text"]').val(function(i, val) {
         alert(val);
     });
 });//this sends out an alert when #btnSubmit is clicked
