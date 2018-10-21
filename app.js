88b4880c17f0bac9750b2b062a88621910166adb
Patrick Hogan <phogan110@live.com>
2018-10-21 04:59:26 -0600

    submit now appends an h2 with submitted text

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+$('#btnSubmit').after('<div></div>')
+
 $('#btnSubmit').attr('disabled', true);
 
-$('input[type="text"]').on('keyup', function() {
-    $('#btnSubmit').attr('disabled', false);
-});
+$('input[type="text"]').keyup(function () {
+    if ($('input[type= "text"]').val() == '') {
+        $('#btnSubmit').attr('disabled', true);
+    } else ($('#btnSubmit').attr('disabled', false))
+}).keyup();
 
-$('#btnSubmit').on('click', function() {
+$('#btnSubmit').click(function(e) {
+    e.preventDefault();
     $('input[type="text"]').val(function(i, val) {
-        alert(val);
+        $('div').append('<h2></h2>');
+        $('h2').append(val)
     });
 });//this sends out an alert when #btnSubmit is clicked
-
-$('#btnSubmit').after('<div></div>')
